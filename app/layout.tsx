1dd2977ff302f9bb35a76a01c8c7dea1adbf2bc2
metzevandro <metzxevandro@gmail.com>
2026-04-09 14:49:03 -0300

    add: adicionado o favicon.ico na pagina principal do navegador quando o site é pesquisado

--- app/layout.tsx
+++ app/layout.tsx
@@ -105,13 +105,15 @@ export default function RootLayout({
       data-scroll-behavior="smooth"
     >
       <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
           crossOrigin="anonymous"
         />
-
         <link
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=swap"
           rel="stylesheet"
